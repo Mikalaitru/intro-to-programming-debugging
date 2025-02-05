84ade7160995aedf4410048606447c711a7af0b7
Mikalai <mikolka27667422@gmail.com>
2025-02-04 20:54:32 -0800

    debugging01

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function checkGuess() {
     if (guess < targetNumber) {
       tooLowMessage.style.display = '';
     } else {
-      tooLowMessage.style.display = '';
+      //should be tooHigh insted toolow
+      tooHighMessage.style.display = '';
     }
 
     const remainingAttempts = maxNumberOfAttempts - attempts;
@@ -51,8 +52,8 @@ function checkGuess() {
     numberOfGuessesMessage.style.display = '';
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
-
-  if (attempts ==== maxNumberOfAttempts) {
+// extra "="
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
@@ -63,21 +64,23 @@ function checkGuess() {
 }
 
 function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
+  for (let elementIndex = 0; elementIndex < messages.length; elementIndex++) {
     messages[elementIndex].style.display = 'none';
   }
 }
-
-funtion setup() {
+// Misspelled "function"
+function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  // I added "<"
+  maxNumberOfAttempts <= 5;
 
   // Enable the input and submit button
-  submitButton.disabeld = false;
+  //misspelled "disabled"
+  submitButton.disabled = false;
   guessInput.disabled = false;
 
   hideAllMessages();
